95c13318a5cf6830fd69a632f9048164a34ec873
jsproject0701-jpg <jsproject0701@gmail.com>
2026-03-16 10:42:21 +0900

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,13 @@
+const SITE_CONFIG = {
+
+  email: "furusato.ongakukai.2018@gmail.com",
+
+  phone: "09071234665",
+
+  twitter: "https://twitter.com/FurusatoOngaku",
+
+  instagram: "https://instagram.com/furusato.ongakukai",
+
+  youtube: "https://youtube.com/@Furusato.ongakukai"
+
+};
